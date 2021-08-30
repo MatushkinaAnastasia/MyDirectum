094c123408915afdc53b92839c5aed95148050c3
Анастасия Матушкина <MatushkinaAnastasia>
2021-08-30 13:44:54 +0500

    Импорт пакета разработки [Rx]

---
+++ Sungero.Docflow/Sungero.Docflow.Shared/CaseFile/CaseFileConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace Sungero.Docflow.Constants
+{
+  public static class CaseFile
+  {
+
+    /// <summary>
+    /// Код справки для диалога копирования файлов.
+    /// </summary>
+    public const string CopyFilesDialogHelpCode = "TODO";
+
+  }
+}
